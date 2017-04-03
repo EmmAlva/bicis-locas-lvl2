7c28ae7c2a76b597ec70ae8df21066ce932889c2
Emma Tapia <clioemma_28@hotmail.com>
2017-04-03 17:12:17 -0500

    Form validated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,74 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var name = document.getElementById("name"),
+		lastname = document.getElementById("lastname"),
+		email = document.getElementById("input-email"),
+		password = document.getElementById("input-password"),
+		select = document.getElementById("select"),  //.selectedIndex;
+		social = document.getElementById("input-social");
+	var span0 = document.getElementById("wrong0"),
+		span1 = document.getElementById("wrong1"),
+		span2 = document.getElementById("wrong2"),
+		span3 = document.getElementById("wrong3"),
+		span4 = document.getElementById("wrong4");
+
+	if(name.value == ""){
+		span0.innerHTML = " Debe ingresar su nombre";
+		name.focus();
+		return false;
+	}
+	else if(/([A-ZÑÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+/.test(name.value) == false){
+		span0.innerHTML = " Primera letra en mayúscula";
+		name.focus();
+		return false;
+	}
+		else{
+			span0.innerHTML ="";
+			lastname.focus();
+		}
+	if(lastname.value == ""){
+		span1.innerHTML = "Debe ingresar su apellido";
+		lastname.focus();
+		return false;
+	}
+	else if(/([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+/.test(lastname.value) == false){
+		span1.innerHTML = " Primera letra en mayúscula";
+		lastname.focus();
+		return false;
+	}
+		else{
+			span1.innerHTML ="";
+			email.focus();
+		}
+	if(/[\w]+@{1}[\w\-]+\.[a-z]{2,3}/.test(email.value)==false){
+		span2.innerHTML = "Verifique su e-mail";
+		email.focus();
+		return false;
+	}
+		else{
+			span2.innerHTML="";
+			password.focus();
+		}
+	if(password.value.length<6){
+		span3.innerHTML ="La contraseña debe tener al menos 6 caractéres";
+		password.focus();
+		return false;
+	}
+	else if(password.value == "password" || password.value == "123456" || password.value == "098754"){
+		span3.innerHTML ="Seguridad de la contraseña: Débil";
+		password.focus();
+		return false;
+	}
+	else{
+		span3.innerHTML= "";
+		select.focus();	
+	}
+	if(select.selectedIndex == ""){
+		span4.innerHTML ="Debe seleccionar al menos un tipo de bici";
+		select.focus();
+		return false;
+	}
+	else{
+		span4.innerHTML ="";
+	}
+	element.reset();
 }
